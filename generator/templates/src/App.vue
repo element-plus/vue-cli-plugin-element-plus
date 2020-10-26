4dc00b9fb2fac2f185be634fe0adfdc7de2ce65d
zazzaz <izazzaz@hotmail.com>
2020-10-26 17:00:33 +0800

    chore: update

--- generator/templates/src/App.vue
+++ generator/templates/src/App.vue
@@ -17,7 +17,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     HelloWorld
   }
@@ -26,7 +26,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- generator/templates/src/plugins/element.js
+++ generator/templates/src/plugins/element.js
@@ -1,27 +1,32 @@
+const hasCustomLocale = options.lang !== 'zh-CN'
 <%_ if (options.import === 'full') { _%>
 import ElementPlus from 'element-plus'
 <%_ if (options.customTheme) { _%>
 import '../element-variables.scss'
 <%_ } else { _%>
 import 'element-plus/lib/theme-chalk/index.css'
-<%_ } _%>
-<%_ if (options.lang !== 'zh-CN') { _%>
+<%_ } _%> 
+<%_ if (hasCustomLocale) { _%>
 import locale from 'element-plus/lib/locale/lang/<%= options.lang %>'
-
-<%_ } else { _%>
-
 <%_ } _%>
 <%_ } else { _%>
 import { Button } from 'element-plus'
-<%_ if (options.lang !== 'zh-CN') { _%>
+<%_ if (hasCustomLocale) { _%>
 import lang from 'element-plus/lib/locale/lang/<%= options.lang %>'
 import locale from 'element-plus/lib/locale'
+<%_ }} _%>
 
-locale.use(lang)
-<%_ } _%>
-
-Vue.use(Button)
-<%_ } _%>
 export default (app) => {
+  <%_ if (options.import === 'full') { _%>
+  <%_ if (hasCustomLocale) { _%>
+  app.use(ElementPlus, { locale })
+  <%_ } else { _%>
   app.use(ElementPlus)
+  <%_ } _%>
+  <%_ } else { _%>
+  <%_ if (hasCustomLocale) { _%>
+  locale.use(lang)
+  <%_ } _%>
+  app.use(Button)
+  <%_ } _%>
 }
