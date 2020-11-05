ee8eaa6b71f94739b5ac2451f29d3d2d69732d2e
zazzaz <izazzaz@hotmail.com>
2020-11-05 15:32:05 +0800

    chore: update

--- generator/templates/src/plugins/element.js
+++ generator/templates/src/plugins/element.js
@@ -5,25 +5,25 @@ import '../element-variables.scss'
 <%_ } else { _%>
 import 'element-plus/lib/theme-chalk/index.css'
 <%_ } _%> 
-<%_ if (options.lang !== 'zh-CN') { _%>
+<%_ if (options.lang !== 'en') { _%>
 import locale from 'element-plus/lib/locale/lang/<%= options.lang %>'
 <%_ } _%>
 <%_ } else { _%>
 import { Button } from 'element-plus'
-<%_ if (options.lang !== 'zh-CN') { _%>
+<%_ if (options.lang !== 'en') { _%>
 import lang from 'element-plus/lib/locale/lang/<%= options.lang %>'
 import locale from 'element-plus/lib/locale'
 <%_ }} _%>
 
 export default (app) => {
   <%_ if (options.import === 'full') { _%>
-  <%_ if (options.lang !== 'zh-CN') { _%>
+  <%_ if (options.lang !== 'en') { _%>
   app.use(ElementPlus, { locale })
   <%_ } else { _%>
   app.use(ElementPlus)
   <%_ } _%>
   <%_ } else { _%>
-  <%_ if (options.lang !== 'zh-CN') { _%>
+  <%_ if (options.lang !== 'en') { _%>
   locale.use(lang)
   <%_ } _%>
   app.use(Button)
--- prompts.js
+++ prompts.js
@@ -21,11 +21,11 @@ module.exports = [
   {
     type: 'list',
     name: 'lang',
-    message: 'Choose the locale you want to load',
+    message: 'Choose the locale you want to load, the default locale is English (en)',
     choices: localeList.map(locale => ({
       name: locale,
       value: locale
     })),
-    default: 'zh-CN'
+    default: 'en'
   }
 ]
